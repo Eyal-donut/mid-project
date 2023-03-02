beaf203545b8e188a4f24a0949132b808eaed8ad
Eyal-donut <LilaEyal@gmail.com>
2023-03-02 15:49:05 +0100

    starting_battle_02

--- src/pages/BattlePage.js
+++ src/pages/BattlePage.js
@@ -1,11 +1,30 @@
 import { Link } from "react-router-dom";
+import { useLocationContext } from "../context/CurrentLocationContext";
+
+import classes from './BattlePage.module.css'
 
 const BattlePage = () => {
+  const {currentLocation} = useLocationContext()
+
+
+
+
   return (
     <>
+    <main
+        className={classes.main}
+        style={{
+          background: `url(${currentLocation.imageUrl}) no-repeat center center/cover`,
+        }}
+      >
       <h1>LET'S GOOOO, fight page</h1>
-      <Link to=".." relative="path"><button>back to location</button></Link>
+      <Link to=".." relative="path"><button>back</button></Link>
       <Link to="/map"><button>Game map</button></Link>
+
+      </main>
+
+
+
     </>
   );
 };
