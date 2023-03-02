12b95364249d6cb41e18aedf93a0d6d9d45f207f
Eyal-donut <LilaEyal@gmail.com>
2023-03-02 10:49:37 +0100

    03_map_done

--- src/Data/LocationsData.js
+++ src/Data/LocationsData.js
@@ -1,24 +1,32 @@
 import classes from '../pages/GameMap.module.css'
+import imageUrlOne from '../assets/backgrounds/mountain2.jpg'
+import imageUrlThree from '../assets/backgrounds/forest.jpg'
+import imageUrlTwo from '../assets/backgrounds/lava.jpg'
+import imageUrlFour from '../assets/backgrounds/beach.jpg'
 
 export const locationsArray = [
     {
-      name: "location 1",
+      name: "The north",
       id: "location-one",
       className: `${classes.one}`,
+      imageUrl: imageUrlOne
     },
     {
-      name: "location 2",
+      name: "Under the mountain",
       id: "location-two",
       className: `${classes.two}`,
+      imageUrl: imageUrlTwo
     },
     {
-      name: "location 3",
+      name: "Pokéforest",
       id: "location-three",
       className: `${classes.three}`,
+      imageUrl: imageUrlThree
     },
     {
-      name: "location 4",
+      name: "Beach",
       id: "location-four",
       className: `${classes.four}`,
+      imageUrl: imageUrlFour
     },
   ];
--- src/pages/LocationPage.js
+++ src/pages/LocationPage.js
@@ -3,26 +3,36 @@ import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { locationsArray } from "../Data/LocationsData";
 import { useLocationContext } from "../context/CurrentLocationContext";
+import classes from "./LocationPage.module.css";
 
 const LocationPage = () => {
-  const {setCurrentLocation} = useLocationContext()
-  const params = useParams()
+  const { currentLocation, setCurrentLocation } = useLocationContext();
+  const params = useParams();
 
   useEffect(() => {
-    const locationByID = locationsArray.find((location)=> location.id === params.locationID)
-    setCurrentLocation(locationByID)
+    const locationByID = locationsArray.find(
+      (location) => location.id === params.locationID
+    );
+    setCurrentLocation(locationByID);
   }, [params.locationID, setCurrentLocation]);
 
   const battleID = 1;
   return (
     <>
-      <h1>This is a location</h1>
-      <Link to={`/map/:locationID/${battleID}`}>
-        <button>Chosen Fight</button>
-      </Link>
-      <Link to=".." relative="path">
-        <button>game map</button>
-      </Link>
+      <main
+        className={classes.main}
+        style={{
+          background: `url(${currentLocation.imageUrl}) no-repeat center center/cover`,
+        }}
+      >
+        <h1>This is a location</h1>
+        <Link to={`/map/:locationID/${battleID}`}>
+          <button>Chosen Fight</button>
+        </Link>
+        <Link to=".." relative="path">
+          <button>game map</button>
+        </Link>
+      </main>
     </>
   );
 };
