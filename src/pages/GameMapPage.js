6a35896dfbe198c795b45215aa4b488b1276e158
Eyal-donut <LilaEyal@gmail.com>
2023-03-02 02:20:29 +0100

    map_done

--- src/pages/GameMapPage.js
+++ src/pages/GameMapPage.js
@@ -1,22 +1,56 @@
 import { Link } from "react-router-dom";
 import classes from "./GameMap.module.css";
 
+const locationsArray = [
+  {
+    name: "location 1",
+    id: "location-one",
+    className: `${classes.one}`,
+  },
+  {
+    name: "location 2",
+    id: "location-two",
+    className: `${classes.two}`,
+  },
+  {
+    name: "location 3",
+    id: "location-three",
+    className: `${classes.three}`,
+  },
+  {
+    name: "location 4",
+    id: "location-four",
+    className: `${classes.four}`,
+  },
+];
+
+
 const GameMapPage = () => {
-  const locationID = 1;
+  
+  const clickHandler = (e) => {
+    console.log(e.target.id);
+  };
+
   return (
     <>
       <main className={classes.main}>
         <div className={classes.cover}>
-          <h1 className="game-map">Game Map</h1>
-          <div className={`${classes.location} ${classes.three}`}>3</div>
-          <div className={`${classes.location} ${classes.four}`}>4</div>
-          <div className={`${classes.location} ${classes.one}`}>1</div>
-          <div className={`${classes.location} ${classes.two}`}>2</div>
+          <h1 className={classes.h1}>Game Map</h1>
+
+          {locationsArray.map((location) => {
+            return (
+              <div key={location.id} className={`${classes.wrapper} ${location.className}`}>
+                <h2 className={classes.h2}>{location.name}</h2>
+                <div
+                  id={location.id}
+                  className={classes.location}
+                  onClick={clickHandler}
+                ></div>
+              </div>
+            );
+          })}
           <Link to="/">
             <button>Home</button>
-          </Link>
-          <Link to={`/map/${locationID}`}>
-            <button>Chosen location</button>
           </Link>
         </div>
       </main>
