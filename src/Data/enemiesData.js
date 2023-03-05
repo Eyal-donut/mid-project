b370a2732f2a5c59ed798a7b9dbb839cc7ffbdc9
Eyal-donut <LilaEyal@gmail.com>
2023-03-05 10:31:40 +0100

    pokemon_links_updated

--- src/Data/enemiesData.js
+++ src/Data/enemiesData.js
@@ -1,57 +1,33 @@
 import classes from "../pages/LocationPage.module.css";
-import zero from "../assets/enemies/0.png";
-import one from "../assets/enemies/1.png";
-import two from "../assets/enemies/2.png";
-import three from "../assets/enemies/3.png";
-import four from "../assets/enemies/4.png";
-import five from "../assets/enemies/5.png";
-import six from "../assets/enemies/6.png";
-import seven from "../assets/enemies/7.png";
-import eight from "../assets/enemies/8.png";
-import nine from "../assets/enemies/9.png";
-import ten from "../assets/enemies/10.png";
-import eleven from "../assets/enemies/11.png";
-import twelve from "../assets/enemies/12.png";
-import thirteen from "../assets/enemies/13.png";
-import fourteen from "../assets/enemies/14.png";
-import fifteen from "../assets/enemies/15.png";
-import sixteen from "../assets/enemies/16.png";
-import seventeen from "../assets/enemies/17.png";
-import eighteen from "../assets/enemies/18.png";
-import nineteen from "../assets/enemies/19.png";
-import twenty from "../assets/enemies/20.png";
-import twentyOne from "../assets/enemies/21.png";
-import twentyTwo from "../assets/enemies/22.png";
-import twentyThree from "../assets/enemies/23.png";
 
 export const enemiesData = [
-  { name: "Onix", id: 0, imageUrl: zero },
-  { name: "Machoke", id: 1, imageUrl: one },
-  { name: "Meowth", id: 2, imageUrl: two },
-  { name: "Psyduck", id: 3, imageUrl: three },
-  { name: "Mankey", id: 4, imageUrl: four },
-  { name: "Beedrill", id: 5, imageUrl: five },
+  { name: "Onix", id: 0, imageUrl: 'https://raw.githubusercontent.com/Eyal-donut/mid-project/main/src/assets/all_pokemons/0.png'},
+  { name: "Machoke", id: 1, imageUrl: 'https://raw.githubusercontent.com/Eyal-donut/mid-project/main/src/assets/all_pokemons/1.png' },
+  { name: "Meowth", id: 2, imageUrl: 'https://raw.githubusercontent.com/Eyal-donut/mid-project/main/src/assets/all_pokemons/2.png' },
+  { name: "Psyduck", id: 3, imageUrl: 'https://raw.githubusercontent.com/Eyal-donut/mid-project/main/src/assets/all_pokemons/3.png' },
+  { name: "Mankey", id: 4, imageUrl: 'https://raw.githubusercontent.com/Eyal-donut/mid-project/main/src/assets/all_pokemons/4.png' },
+  { name: "Beedrill", id: 5, imageUrl: 'https://raw.githubusercontent.com/Eyal-donut/mid-project/main/src/assets/all_pokemons/5.png' },
 
-  { name: "Charizard", id: 6, imageUrl: six },
-  { name: "Rapidash", id: 7, imageUrl: seven },
-  { name: "Ninetales", id: 8, imageUrl: eight },
-  { name: "Charmander", id: 9, imageUrl: nine },
-  { name: "Arcanine", id: 10, imageUrl: ten },
-  { name: "Magmar", id: 11, imageUrl: eleven },
+  { name: "Charizard", id: 6, imageUrl: 'https://raw.githubusercontent.com/Eyal-donut/mid-project/main/src/assets/all_pokemons/6.png' },
+  { name: "Rapidash", id: 7, imageUrl: 'https://raw.githubusercontent.com/Eyal-donut/mid-project/main/src/assets/all_pokemons/7.png' },
+  { name: "Ninetales", id: 8, imageUrl: 'https://raw.githubusercontent.com/Eyal-donut/mid-project/main/src/assets/all_pokemons/8.png' },
+  { name: "Charmander", id: 9, imageUrl: 'https://raw.githubusercontent.com/Eyal-donut/mid-project/main/src/assets/all_pokemons/9.png' },
+  { name: "Arcanine", id: 10, imageUrl: 'https://raw.githubusercontent.com/Eyal-donut/mid-project/main/src/assets/all_pokemons/10.png' },
+  { name: "Magmar", id: 11, imageUrl: 'https://raw.githubusercontent.com/Eyal-donut/mid-project/main/src/assets/all_pokemons/11.png' },
 
-  { name: "Gyarados", id: 12, imageUrl: twelve },
-  { name: "Poliwhirl", id: 13, imageUrl: thirteen },
-  { name: "Tentacruel", id: 14, imageUrl: fourteen },
-  { name: "Vaporeon", id: 15, imageUrl: fifteen },
-  { name: "Lapras", id: 16, imageUrl: sixteen },
-  { name: "Dragonair", id: 17, imageUrl: seventeen },
+  { name: "Gyarados", id: 12, imageUrl: 'https://raw.githubusercontent.com/Eyal-donut/mid-project/main/src/assets/all_pokemons/12.png' },
+  { name: "Poliwhirl", id: 13, imageUrl: 'https://raw.githubusercontent.com/Eyal-donut/mid-project/main/src/assets/all_pokemons/13.png' },
+  { name: "Tentacruel", id: 14, imageUrl: 'https://raw.githubusercontent.com/Eyal-donut/mid-project/main/src/assets/all_pokemons/14.png' },
+  { name: "Vaporeon", id: 15, imageUrl: 'https://raw.githubusercontent.com/Eyal-donut/mid-project/main/src/assets/all_pokemons/15.png' },
+  { name: "Lapras", id: 16, imageUrl: 'https://raw.githubusercontent.com/Eyal-donut/mid-project/main/src/assets/all_pokemons/16.png' },
+  { name: "Dragonair", id: 17, imageUrl: 'https://raw.githubusercontent.com/Eyal-donut/mid-project/main/src/assets/all_pokemons/17.png' },
 
-  { name: "Mewtwo", id: 18, imageUrl: eighteen },
-  { name: "Raichu", id: 19, imageUrl: nineteen },
-  { name: "Alakazam", id: 20, imageUrl: twenty },
-  { name: "Arbok", id: 21, imageUrl: twentyOne },
-  { name: "Jigglypuff", id: 22, imageUrl: twentyTwo },
-  { name: "Weepinbell", id: 23, imageUrl: twentyThree },
+  { name: "Mewtwo", id: 18, imageUrl: 'https://raw.githubusercontent.com/Eyal-donut/mid-project/main/src/assets/all_pokemons/18.png' },
+  { name: "Raichu", id: 19, imageUrl: 'https://raw.githubusercontent.com/Eyal-donut/mid-project/main/src/assets/all_pokemons/19.png' },
+  { name: "Alakazam", id: 20, imageUrl: 'https://raw.githubusercontent.com/Eyal-donut/mid-project/main/src/assets/all_pokemons/20.png' },
+  { name: "Arbok", id: 21, imageUrl: 'https://raw.githubusercontent.com/Eyal-donut/mid-project/main/src/assets/all_pokemons/21.png' },
+  { name: "Jigglypuff", id: 22, imageUrl: 'https://raw.githubusercontent.com/Eyal-donut/mid-project/main/src/assets/all_pokemons/22.png' },
+  { name: "Weepinbell", id: 23, imageUrl: 'https://raw.githubusercontent.com/Eyal-donut/mid-project/main/src/assets/all_pokemons/23.png' },
 ];
 
  export const testEnemy = {
@@ -66,5 +42,5 @@ export const enemiesData = [
    healthPoints: 100,
    xpReward: 5,
    location: "location-one",
-   imageUrl: zero,
+   imageUrl: 'https://github.com/Eyal-donut/mid-project/blob/main/src/assets/all_pokemons/0.png',
  };
